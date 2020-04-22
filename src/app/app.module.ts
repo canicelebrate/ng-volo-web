21a30959613fa39c94a39ac202d9b67cb937a078
william_wang <william_wang@trimble.com>
2020-04-22 17:29:40 +0800

    ng-alain+abocore see login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,104 @@
+// tslint:disable: no-duplicate-imports
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, Injector, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+// #region default language
+// Reference: https://ng-alain.com/docs/i18n
+import { default as ngLang } from '@angular/common/locales/zh';
+import { DELON_LOCALE, zh_CN as delonLang } from '@delon/theme';
+import { zhCN as dateLang } from 'date-fns/locale';
+import { NZ_DATE_LOCALE, NZ_I18N, zh_CN as zorroLang } from 'ng-zorro-antd/i18n';
+const LANG = {
+  abbr: 'zh',
+  ng: ngLang,
+  zorro: zorroLang,
+  date: dateLang,
+  delon: delonLang,
+};
+// register angular
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(LANG.ng, LANG.abbr);
+const LANG_PROVIDES = [
+  { provide: LOCALE_ID, useValue: LANG.abbr },
+  { provide: NZ_I18N, useValue: LANG.zorro },
+  { provide: NZ_DATE_LOCALE, useValue: LANG.date },
+  { provide: DELON_LOCALE, useValue: LANG.delon },
+];
+// #endregion
+
+// #region JSON Schema form (using @delon/form)
+import { JsonSchemaModule } from '@shared';
+const FORM_MODULES = [JsonSchemaModule];
+// #endregion
+
+// #region Http Interceptors
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { DefaultInterceptor } from '@core';
+import { SimpleInterceptor } from '@delon/auth';
+const INTERCEPTOR_PROVIDES = [
+  // { provide: HTTP_INTERCEPTORS, useClass: SimpleInterceptor, multi: true },
+  // { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
+];
+// #endregion
+
+// #region global third module
+import { environment } from '../environments/environment';
+
+import { CoreModule as abpCoreModule } from '@abp/ng.core';
+
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+
+const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
+
+const GLOBAL_THIRD_MODULES = [
+  abpCoreModule.forRoot({
+    environment,
+  }),
+  NgxsModule.forRoot(),
+];
+// #endregion
+
+// #region Startup Service
+import { StartupService } from '@core';
+export function StartupServiceFactory(startupService: StartupService) {
+  return () => startupService.load();
+}
+const APPINIT_PROVIDES = [
+  StartupService,
+  {
+    provide: APP_INITIALIZER,
+    useFactory: StartupServiceFactory,
+    deps: [StartupService],
+    multi: true,
+  },
+];
+// #endregion
+
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DelonModule } from './delon.module';
+import { LayoutModule } from './layout/layout.module';
+import { RoutesModule } from './routes/routes.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    DelonModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    LayoutModule,
+    RoutesModule,
+    ...FORM_MODULES,
+    ...GLOBAL_THIRD_MODULES,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [...LANG_PROVIDES, ...INTERCEPTOR_PROVIDES, ...APPINIT_PROVIDES],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,37 @@
 // This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+// This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  SERVER_URL: `./`,
+  hmr: false,
+  useHash: true,
+  production: false,
+  application: {
+    name: 'znext',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44327',
+    clientId: 'znext_App',
+    dummyClientSecret: '1q2w3e*',
+    scope: 'znext',
+    showDebugInformation: true,
+    oidc: false,
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44327',
+    },
+  },
+  localization: {
+    defaultResourceName: 'znext',
+  },
 };
 
 /*
@@ -12,5 +40,13 @@ export const environment = {
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
